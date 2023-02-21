88500332b32407ec15cc6c153a5b4fd8fa8ed68c
Molly Donegan <mollyrdonegan@gmail.com>
2023-02-21 13:49:51 -0800

    refactor tests

--- Anagram.Tests/GameTests.cs
+++ Anagram.Tests/GameTests.cs
@@ -12,7 +12,7 @@ namespace Anagram.Tests
     [TestMethod]
     public void GameConstructor_CreatesInstanceOfGame_Game()
     {
-      Game newGame = new Game("bread");
+      Game newGame = new Game("bread", "beard");
       Assert.AreEqual(typeof(Game), newGame.GetType());
     }
 
@@ -20,9 +20,12 @@ namespace Anagram.Tests
     public void GetResponse_ReturnsWordAsString_String()
     {
       string word1 = "bread";
-      Game newGame = new Game(word1);
-      string result = newGame.FirstWord;
-      Assert.AreEqual(word1, result);
+      string word2 = "beard";
+      Game newGame = new Game(word1, word2);
+      string result1 = newGame.FirstWord;
+      string result2 = newGame.SecondWord;
+      Assert.AreEqual(word1, result1);
+      Assert.AreEqual(word2, result2);
     }
 
     // [TestMethod]
@@ -39,7 +42,8 @@ namespace Anagram.Tests
     public void SortArray_ReturnSortedArray_Array()
     {
       string word1 = "bread";
-      Game newGame = new Game(word1);
+      string word2 = "beard";
+      Game newGame = new Game(word1, word2);
       char[] testArray = {'a', 'b', 'd', 'e', 'r'};
       char[] result = newGame.CheckWord();
       CollectionAssert.AreEqual(testArray, result);
