183cbe9cd55d27ba6a4e80e149c136338a22725a
Molly Donegan <mollyrdonegan@gmail.com>
2023-02-21 09:52:22 -0800

    pass ReturnsWordAsString test

--- Anagram.Tests/GameTests.cs
+++ Anagram.Tests/GameTests.cs
@@ -1,5 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Anagram.Models;
+using System;
+using System.Collections.Generic;
 
 namespace Anagram.Tests
 
@@ -10,8 +12,17 @@ namespace Anagram.Tests
     [TestMethod]
     public void GameConstructor_CreatesInstanceOfGame_Game()
     {
-      Game newGame = new Game();
+      Game newGame = new Game("bread");
       Assert.AreEqual(typeof(Game), newGame.GetType());
+    }
+
+    [TestMethod]
+    public void GetResponse_ReturnsWordAsString_String()
+    {
+      string word1 = "bread";
+      Game newGame = new Game(word1);
+      string result = newGame.FirstWord;
+      Assert.AreEqual(word1, result);
     }
   }
 }
--- Anagram/Models/Game.cs
+++ Anagram/Models/Game.cs
@@ -1,11 +1,20 @@
-using System;
+using System.Collections.Generic;
+
 namespace Anagram.Models
+
 {
   public class Game
   {
-    public Game()
+    private string _firstWord;
+    public string FirstWord
     {
-
+      get { return _firstWord; }
+      set { _firstWord = value; }
     }
+    public Game(string word1)
+    {
+      _firstWord = word1;
+    }
+
   }
 }
