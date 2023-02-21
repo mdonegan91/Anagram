527927c1c84193db9a9403c91b846272d136b512
Molly Donegan <mollyrdonegan@gmail.com>
2023-02-21 15:32:48 -0800

    WIP

--- Anagram.Tests/GameTests.cs
+++ Anagram.Tests/GameTests.cs
@@ -9,24 +9,24 @@ namespace Anagram.Tests
   [TestClass]
   public class GameTests
   {
-    [TestMethod]
-    public void GameConstructor_CreatesInstanceOfGame_Game()
-    {
-      Game newGame = new Game("bread", "beard");
-      Assert.AreEqual(typeof(Game), newGame.GetType());
-    }
+    // [TestMethod]
+    // public void GameConstructor_CreatesInstanceOfGame_Game()
+    // {
+    //   Game newGame = new Game("bread", "beard");
+    //   Assert.AreEqual(typeof(Game), newGame.GetType());
+    // }
 
-    [TestMethod]
-    public void GetResponse_ReturnsWordAsString_String()
-    {
-      string word1 = "bread";
-      string word2 = "beard";
-      Game newGame = new Game(word1, word2);
-      string result1 = newGame.FirstWord;
-      string result2 = newGame.SecondWord;
-      Assert.AreEqual(word1, result1);
-      Assert.AreEqual(word2, result2);
-    }
+    // [TestMethod]
+    // public void GetResponse_ReturnsWordAsString_String()
+    // {
+    //   string word1 = "bread";
+    //   string word2 = "beard";
+    //   Game newGame = new Game(word1, word2);
+    //   string result1 = newGame.FirstWord;
+    //   string result2 = newGame.SecondWord;
+    //   Assert.AreEqual(word1, result1);
+    //   Assert.AreEqual(word2, result2);
+    // }
 
     // [TestMethod]
     // public void GetArray_ReturnsStringAsArray_Array()
@@ -50,16 +50,29 @@ namespace Anagram.Tests
     // }
 
     [TestMethod]
-    public void CheckAnagram_DeterminesWhenAnagram_Array()
+    public void CheckAnagram_DeterminesWhenAnagram_String()
     {
       string word1 = "bread";
       string word2 = "beard";
-      Game newGame = new Game(word1, word2);
-      // char[] resultArray1 = newGame.CheckWord(word1);
-      // char[] resultArray2 = newGame.CheckWord(word2);
-      string result = newGame.CheckWord(word1, word2);
-      Assert.AreEqual("true", result);
+      string word3 = "cheese";
+      Game newGame = new Game(word1, word2, word3);
+      string result = newGame.CheckWord(word1, word2, word3);
+      Assert.AreEqual("one anagram found", result);
+    }
 
+    [TestMethod]
+    public void CheckMultipleAnagrams_ReturnWordMatch_String()
+    {
+      string word1 = "bread";
+      string word2 = "beard";
+      string word3 = "cheese";
+      Game newGame = new Game(word1, word2, word3);
+      string result1 = newGame.CheckWord(word1, word2);
+      string result2 = newGame.CheckWord(word1, word3);
+      string result3 = newGame.CheckWord(word1, word2, word3);
+      Assert.AreEqual("one anagram found", result1);
+      Assert.AreEqual("no anagrams found", result2);
+      Assert.AreEqual("all words are anagrams", result3);
     }
   }
 }
